fbefc3e522420d67f8a58c502a5871818ac36249
Brendan Deery <156530572+bdeery002@users.noreply.github.com>
2026-08-07 03:11:59 +0000

    h

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.conf import settings
-from django.views.static import serve
-from django.urls import include, path, re_path
 from django.contrib import admin
+from django.urls import include, path
 from django_ratelimit.decorators import ratelimit
 
 admin.site.login = ratelimit(key="ip", rate="5/m")(admin.site.login)
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,13 +8,11 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-django_app = get_wsgi_application()
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-application = WhiteNoise(django_app, root=str(BASE_DIR / "staticfiles"))
+application = get_wsgi_application()
+application = WhiteNoise(application, root="/app/staticfiles")
+application.add_files("/app/staticfiles", prefix="static/")
