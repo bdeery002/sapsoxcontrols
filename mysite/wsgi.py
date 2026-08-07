9b6bb586afefca57730419eaddae5f057ee829fb
Brendan Deery <156530572+bdeery002@users.noreply.github.com>
2026-08-07 02:29:48 +0000

    hopefully

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_wsgi_application()
+django_app = get_wsgi_application()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+application = WhiteNoise(django_app, root=str(BASE_DIR / "staticfiles"))
