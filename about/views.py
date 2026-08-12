a0d8397a621aa778385ae98320894cf5e4323746
Brendan Deery <156530572+bdeery002@users.noreply.github.com>
2026-08-12 20:26:52 +0000

    updates

--- about/views.py
+++ about/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from mysite.constants import TEMPLATE_REGISTRY as T
+from sox_controls.models import BusinessProcess
 
 
 @require_http_methods(["GET"])
 def about(request):
     """Render the About page."""
-    return render(request, T["ABOUT_INDEX"]["path"])
+    processes = BusinessProcess.objects.all()
+    return render(request, T["ABOUT_INDEX"]["path"], {"processes": processes})
